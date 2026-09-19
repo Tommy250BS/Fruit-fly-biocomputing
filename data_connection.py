d4f8f7386c5f3e90741984cabdbd5967d7c3fc14
Tommy <145769205+Tommy250BS@users.noreply.github.com>
2026-09-19 15:17:52 +0200

    Clean up data_connection.py by removing comments
    Removed comments and unnecessary lines from the data_connection.py file.

--- data_connection.py
+++ data_connection.py
@@ -1,21 +1,15 @@
 from neuprint import Client, fetch_adjacencies, fetch_neurons
 
-# 1. Ci colleghiamo al database
 token = "9f913383dbd800f5078cdf27745a225f5accfff5661b4aa8b975ac7ab3322e83"
 client = Client(
     "https://neuprint.janelia.org", dataset="male-cns:v1.0", token=token
 )
 
-# 2. Scarichiamo le annotazioni dei neuroni di tipo "DNge104"
 neurons, syndist = fetch_neurons("DNge104")
-
-# 3. Scarichiamo le connessioni in USCITA (outgoing) e in ENTRATA (incoming)
 outgoing_edges, neuron_info = fetch_adjacencies("DNge104")
 incoming_edges, neuron_info2 = fetch_adjacencies(None, "DNge104")
 
-# Vediamo cosa c'è dentro:
 print("--- Neuroni trovati ---")
 print(neurons[["bodyId", "type", "instance"]])
-
 print("\n--- Connessioni in USCITA (verso chi mandano segnali) ---")
 print(outgoing_edges.head())
